6fd6ffbdd796d63b1e361dcca64bfe06e61c1646
Guillermofdz27 <88850960+Guillermofdz27@users.noreply.github.com>
2021-08-12 23:21:34 -0500

    Update exercise.py

--- assignments/01OperacionesBasicas/src/exercise.py
+++ assignments/01OperacionesBasicas/src/exercise.py
@@ -1,5 +1,15 @@
 def main():
     #escribe tu código abajo de esta línea
+    print("Dame un numero:")
+    entrada1= input()
+    print("Dame otro numero:")
+    entrada2= input()
+    suma= int(entrada1) + int(entrada2)
+    resta= int(entrada1) - int(entrada2)
+    multiplicacion= int(entrada1) * int(entrada2)
+    print( "Suma:" + str(suma))
+    print( "Resta:" + str(resta))
+    print( "Multiplicación:" + str(multiplicacion)
     pass
 
 
